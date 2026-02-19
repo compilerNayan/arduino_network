1b5fa86c871dcb248dc3ec080543d457474029e4
Nayan Kurude <compilerNayan@gmail.com>
2026-02-20 01:10:39 +0530

    kdsajkd

---
+++ internal/04-thread/01-variant/01-WiFiHealthCheckerThread.h
@@ -0,0 +1,24 @@
+#ifndef WIFIHEALTHCHECKERTHREAD_H
+#define WIFIHEALTHCHECKERTHREAD_H
+
+#include <StandardDefines.h>
+#include <IRunnable.h>
+#include <OSAL_Core.h>
+
+#include "../../02-wifi/00-public/02-IWifiConnectionManager.h"
+
+/**
+ * Periodically ensures network connectivity, then sleeps (e.g. for use in a thread loop).
+ */
+/* @Component */
+class WiFiHealthCheckerThread : public IRunnable {
+    /* @Autowired */
+    Private IWifiConnectionManagerPtr wifiConnectionManager;
+
+    Public Void Run() override {
+        wifiConnectionManager->EnsureNetworkConnectivity();
+        OSAL_DelayMs(2000);
+    }
+};
+
+#endif // WIFIHEALTHCHECKERTHREAD_H
---
+++ internal/04-thread/01-variant/02-InternetHealthCheckerThread.h
@@ -0,0 +1,24 @@
+#ifndef INTERNETHEALTHCHECKERTHREAD_H
+#define INTERNETHEALTHCHECKERTHREAD_H
+
+#include <StandardDefines.h>
+#include <IRunnable.h>
+#include <OSAL_Core.h>
+
+#include "../../03-internet/00-public/02-IInternetConnectionManager.h"
+
+/**
+ * Periodically verifies internet connectivity, then sleeps (e.g. for use in a thread loop).
+ */
+/* @Component */
+class InternetHealthCheckerThread : public IInternetHealthCheckerThread {
+    /* @Autowired */
+    Private IInternetConnectionManagerPtr internetConnectionManager;
+
+    Public Void Run() override {
+        internetConnectionManager->VerifyInternetConnectivity();
+        OSAL_DelayMs(5000);
+    }
+};
+
+#endif // INTERNETHEALTHCHECKERTHREAD_H
