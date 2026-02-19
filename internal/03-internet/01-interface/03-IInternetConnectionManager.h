e234e6196af6e9949c429fb423382319759e526f
Nayan Kurude <compilerNayan@gmail.com>
2026-02-20 00:46:26 +0530

    ldsakl

---
+++ internal/03-internet/01-interface/03-IInternetConnectionManager.h
@@ -0,0 +1,26 @@
+#ifndef IINTERNETCONNECTIONMANAGER_H
+#define IINTERNETCONNECTIONMANAGER_H
+
+#include <StandardDefines.h>
+
+/**
+ * Internet/network connection: same as IWifiConnectionManager plus internet reachability.
+ */
+DefineStandardPointers(IInternetConnectionManager)
+class IInternetConnectionManager {
+    Public Virtual ~IInternetConnectionManager() = default;
+
+    Public Virtual Void ConnectNetwork() = 0;
+    Public Virtual Void DisconnectNetwork() = 0;
+    Public Virtual Bool IsNetworkConnected() = 0;
+    Public Virtual Bool IsWifiConnected() = 0;
+    Public Virtual Bool IsHotspotConnected() = 0;
+    Public Virtual Bool IsInternetConnected() = 0;
+    Public Virtual ULong GetWifiConnectionId() = 0;
+    Public Virtual ULong GetNetworkConnectionId() = 0;
+    Public Virtual Bool EnsureNetworkConnectivity() = 0;
+    Public Virtual Bool EnsureInternetConnectivity() = 0;
+    Public Virtual Void RestartNetwork() = 0;
+};
+
+#endif // IINTERNETCONNECTIONMANAGER_H
---
+++ internal/03-internet/02-variant/03-InternetConnectionManager.h
@@ -0,0 +1,123 @@
+#ifdef ARDUINO
+#ifndef INTERNETCONNECTIONMANAGER_H
+#define INTERNETCONNECTIONMANAGER_H
+
+#include "../01-interface/03-IInternetConnectionManager.h"
+#include "../01-interface/01-IInternetConnectionStatusStore.h"
+#include "../../02-wifi/01-interface/03-IWifiConnectionManager.h"
+#include <StandardDefines.h>
+#include <ILogger.h>
+#include <OSAL_Core.h>
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>
+#else
+#include <WiFi.h>
+#include <WiFiClient.h>
+#endif
+
+namespace {
+struct InternetCheckPair {
+    const char* ip1;
+    const char* ip2;
+};
+const InternetCheckPair kInternetCheckPairs[] = {
+    { "208.67.222.123", "1.0.0.1" },       // FamilyShield, Cloudflare
+    { "8.8.4.4", "208.67.220.220" },       // Google, OpenDNS
+    { "156.154.70.1", "64.6.64.6" },       // Neustar, Verisign
+    { "195.46.39.39", "199.85.127.10" },   // SafeDNS, Norton
+    { "76.76.2.0", "94.140.15.15" },       // Control D, AdGuard
+    { "208.67.220.123", "156.154.71.1" },   // FamilyShield, Neustar
+    { "1.1.1.1", "208.67.222.222" },       // Cloudflare, OpenDNS
+    { "8.8.8.8", "64.6.65.6" },            // Google, Verisign
+    { "195.46.39.40", "76.76.10.0" },      // SafeDNS, Control D
+    { "199.85.126.10", "76.76.19.19" },    // Norton, Alternate
+};
+const size_t kNumInternetCheckPairs = sizeof(kInternetCheckPairs) / sizeof(kInternetCheckPairs[0]);
+}
+
+/* @Component */
+class InternetConnectionManager : public IInternetConnectionManager {
+    /* @Autowired */
+    Private IWifiConnectionManagerPtr wifiConnectionManager;
+    /* @Autowired */
+    Private IInternetConnectionStatusStorePtr internetStatusStore;
+    /* @Autowired */
+    Private ILoggerPtr logger;
+
+    Private size_t nextInternetCheckPairIndex_ = 0;
+
+    /** Check internet reachability via DNS server TCP connect (port 53). Updates store with result. */
+    Private Bool HasInternet() {
+        const InternetCheckPair& pair = kInternetCheckPairs[nextInternetCheckPairIndex_];
+        nextInternetCheckPairIndex_ = (nextInternetCheckPairIndex_ + 1) % kNumInternetCheckPairs;
+
+        WiFiClient client;
+        if (client.connect(pair.ip1, 53, 2000)) {
+            client.stop();
+            internetStatusStore->SetState(true, OSAL_GenerateConnectionId());
+            return true;
+        }
+        logger->Warning(Tag::Untagged, StdString("[InternetConnection] Internet check failed for " + StdString(pair.ip1)));
+        if (client.connect(pair.ip2, 53, 2000)) {
+            client.stop();
+            internetStatusStore->SetState(true, OSAL_GenerateConnectionId());
+            return true;
+        }
+        logger->Warning(Tag::Untagged, StdString("[InternetConnection] Internet check failed for " + StdString(pair.ip2)));
+        internetStatusStore->SetState(false, 0);
+        return false;
+    }
+
+    Public Virtual ~InternetConnectionManager() = default;
+
+    Public Virtual Void ConnectNetwork() override {
+        wifiConnectionManager->ConnectNetwork();
+    }
+
+    Public Virtual Void DisconnectNetwork() override {
+        wifiConnectionManager->DisconnectNetwork();
+        internetStatusStore->SetState(false, 0);
+    }
+
+    Public Virtual Bool IsNetworkConnected() override {
+        return wifiConnectionManager->IsNetworkConnected();
+    }
+
+    Public Virtual Bool IsWifiConnected() override {
+        return wifiConnectionManager->IsWifiConnected();
+    }
+
+    Public Virtual Bool IsHotspotConnected() override {
+        return wifiConnectionManager->IsHotspotConnected();
+    }
+
+    Public Virtual Bool IsInternetConnected() override {
+        if (!wifiConnectionManager->IsNetworkConnected()) return false;
+        return HasInternet();
+    }
+
+    Public Virtual ULong GetWifiConnectionId() override {
+        return wifiConnectionManager->GetWifiConnectionId();
+    }
+
+    Public Virtual ULong GetNetworkConnectionId() override {
+        return wifiConnectionManager->GetNetworkConnectionId();
+    }
+
+    Public Virtual Bool EnsureNetworkConnectivity() override {
+        return wifiConnectionManager->EnsureNetworkConnectivity();
+    }
+
+    Public Virtual Bool EnsureInternetConnectivity() override {
+        if (!wifiConnectionManager->EnsureNetworkConnectivity()) return false;
+        return HasInternet();
+    }
+
+    Public Virtual Void RestartNetwork() override {
+        wifiConnectionManager->RestartNetwork();
+        internetStatusStore->SetState(false, 0);
+    }
+};
+
+#endif // INTERNETCONNECTIONMANAGER_H
+#endif // ARDUINO
